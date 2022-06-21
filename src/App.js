fabd56f7ca131962dbfe67ee61cf49cdd5ad3168
saarahchairi <sarni555@hotmail.com>
2022-06-21 16:26:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    let date = new Date();
+    let jour = date.getDate();
+    let mois = date.getMonth();
+    let annee = date.getFullYear();
+    let heure = date.getHours();
+    let minute = date.getMinutes();
+    let seconde = date.getSeconds();
+    return(
+      <div>
+        <h1>Hello, aujourd'hui nous somme le {jour}/{mois}/{annee}. Et il est {heure}h{minute}:{seconde}sec</h1>
+      </div>
+    )
+  };
 }
 
 export default App;
